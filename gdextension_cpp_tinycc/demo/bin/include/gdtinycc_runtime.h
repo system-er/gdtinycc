2ac4a1906df063f3a483f765fe6057a73e9c31f1
systemerror <sys_temerror@web.de>
2026-03-07 14:30:59 +0100

    Add files via upload

--- gdextension_cpp_tinycc/demo/bin/include/gdtinycc_runtime.h
+++ gdextension_cpp_tinycc/demo/bin/include/gdtinycc_runtime.h
@@ -2,5 +2,23 @@
 #define GDTINYCC_RUNTIME_H
 
 void godot_print(const char *msg);
+void* godot_get_node(const char *path);
+const char* godot_get_property(void* node, const char *property);
+
+typedef struct {
+    int type;
+    union {
+        int i;
+        float f;
+        char s[256];
+        int b;
+        float vec2[2];
+        float vec3[3];
+    } value;
+} GDExtensionVariant;
+
+GDExtensionVariant godot_get_variant(void* node, const char* property);
+void godot_print_float(float f);
+const char* godot_get_type_name(int type);
 
 #endif
