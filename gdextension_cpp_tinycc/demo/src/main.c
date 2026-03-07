9eba856c71762e810f93bda16abe3c4defcafc7b
systemerror <sys_temerror@web.de>
2026-03-07 13:09:37 +0100

    Add files via upload

--- gdextension_cpp_tinycc/demo/src/main.c
+++ gdextension_cpp_tinycc/demo/src/main.c
@@ -1,10 +1,21 @@
 
+#include "stddef.h"
 #include "gdtinycc_runtime.h"
 
 void main() {
     godot_print("hello world from gdtinycc");
+    void* testnode = godot_get_node("/root/Main");
+    if (testnode != NULL) {
+        godot_print("gdtinycc main: node found");
+    }
+    else {
+        godot_print("gdtinycc main: node not found");
+    }
 }
 
 void _ready() {
     godot_print("gdtinycc-_ready called!");
+}
+
+void _process(double delta) {
 }
