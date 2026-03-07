e6d29c5638b6a62951e00219af41f895c24bad36
systemerror <sys_temerror@web.de>
2026-03-07 17:03:48 +0100

    Add files via upload

--- gdextension_cpp_tinycc/demo/bin/include/gdtinycc_runtime.h
+++ gdextension_cpp_tinycc/demo/bin/include/gdtinycc_runtime.h
@@ -36,7 +36,23 @@ typedef struct {
     } value;
 } GDExtensionVariant;
 
+typedef enum {
+    VARTYPE_NULL = 0,
+    VARTYPE_BOOL = 1,
+    VARTYPE_INT = 2,
+    VARTYPE_FLOAT = 3,
+    VARTYPE_STRING = 4,
+    VARTYPE_VECTOR2 = 5,
+    VARTYPE_VECTOR3 = 6,
+    VARTYPE_COLOR = 7
+} GDExtensionVarType;
+
 GDExtensionVariant godot_get_variant(void* node, const char* property);
+void godot_set_variant(void* node, const char* property, GDExtensionVariant variant);
+void* godot_instantiate(const char* scene_path);
+void* godot_create(const char* class_name);
+void godot_add_child(void* parent, void* child);
+void godot_add_child_deferred(void* parent, void* child);
 void godot_print_float(float f);
 const char* godot_get_type_name(int type);
 
