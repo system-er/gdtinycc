fbbe881afbf4c2c39f1fd919b5f0a1e462938bab
systemerror <sys_temerror@web.de>
2026-03-08 10:24:02 +0100

    Add files via upload

--- gdextension_cpp_tinycc/demo/src/main.c
+++ gdextension_cpp_tinycc/demo/src/main.c
@@ -2,17 +2,22 @@
 #include "stddef.h"
 #include "gdtinycc_runtime.h"
 
+
+void on_button_pressed(void* user_data, GDExtensionVariant arg) {
+    godot_print("Button pressed!");
+}
+
 void main() {
-    godot_print("hello world from gdtinycc");
+    godot_print("hello world from GDTinyCC.");
     void* testnode = godot_get_node("/root/Main");
     if (testnode != NULL) {
-        godot_print("gdtinycc main: node found");
+        godot_print("GDTinyCC main: node found");
         GDExtensionVariant v = godot_get_variant(testnode, "name");
         godot_print(godot_get_type_name(v.type));
         godot_print(v.value.s);
     }
     else {
-        godot_print("gdtinycc main: node not found");
+        godot_print("GDTinyCC main: node not found");
     }
 
     void *label = godot_create("Label");
@@ -22,23 +27,34 @@ void main() {
     }
     GDExtensionVariant v;
     v.type = VARTYPE_STRING;
-    snprintf(v.value.s, sizeof(v.value.s), "this is a label from gdtinycc");
+    snprintf(v.value.s, sizeof(v.value.s), "this is a label from GDTinyCC");
     godot_set_variant(label, "text", v);
 
     GDExtensionVariant va;
     va.type = VARTYPE_VECTOR2;
     va.value.vec2.x = 200.0f;
-    va.value.vec2.y = 100.0f;
+    va.value.vec2.y = 20.0f;
     godot_set_variant(label, "position", va);
 
     void *main_node = godot_get_node("/root/Main");
     godot_add_child_deferred(main_node, label);
 
+    void* button = godot_get_node("/root/Main/Button");
+    godot_connect(button, "pressed", on_button_pressed, NULL);
+
 }
 
 void _ready() {
-    godot_print("gdtinycc-_ready called!");
+    godot_print("GDTinyCC _ready called!");
 }
 
 void _process(double delta) {
+}
+
+void _physics_process(double delta) {
+}
+
+void _input(void* event_ptr)
+{
+    //godot_print("input event!");
 }
