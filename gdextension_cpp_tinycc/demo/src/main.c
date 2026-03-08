6e2f0fa0ef45864b2ef050c47100acc3f734afd5
systemerror <sys_temerror@web.de>
2026-03-08 13:42:06 +0100

    Add files via upload

--- gdextension_cpp_tinycc/demo/src/main.c
+++ gdextension_cpp_tinycc/demo/src/main.c
@@ -1,51 +1,76 @@
-
+// GDTinyCC test-c-program https://github.com/system-er/gdtinycc/tree/main
 #include "stddef.h"
 #include "gdtinycc_runtime.h"
 
 
+// callfunction for button-signal
 void on_button_pressed(void* user_data, GDExtensionVariant arg) {
     godot_print("Button pressed!");
 }
 
+// callfunction for timer-signal
 void on_timeout(void* user_data) {
     godot_print("timeout!");
 }
 
+// a little benchmark
+int benchmark() {
+	int sum = 0;
+	int n1 = 0;
+	int n2 = 1;
+    int n = 0;
+	for(int i=0; i<10000000; i++) {
+		n = n2;
+		n2 = n2 + n1;
+		n1 = n;
+		sum = sum + n2;
+    }
+
+	return sum;
+}
+
+// the c-main-function
 void main() {
     godot_print("hello world from GDTinyCC.");
-    void* testnode = godot_get_node("/root/Main");
-    if (testnode != NULL) {
+
+    // get the parentnode
+    void* parent = godot_get_node("/root/Main");
+    if (parent != NULL) {
         godot_print("GDTinyCC main: node found");
-        GDExtensionVariant v = godot_get_variant(testnode, "name");
+        GDExtensionVariant v = godot_get_variant(parent, "name");
+        // show the type of the name
         godot_print(godot_get_type_name(v.type));
+        // show the name of the parentnode
         godot_print(v.value.s);
     }
     else {
         godot_print("GDTinyCC main: node not found");
     }
 
+    // create a label from code
     void *label = godot_create("Label");
     if (!label) {
         godot_print("failed to create label");
         return;
     }
+    // set labeltext
     GDExtensionVariant v;
     v.type = VARTYPE_STRING;
     snprintf(v.value.s, sizeof(v.value.s), "this is a label from GDTinyCC");
     godot_set_variant(label, "text", v);
-
+    // set labelposition
     GDExtensionVariant va;
     va.type = VARTYPE_VECTOR2;
     va.value.vec2.x = 200.0f;
     va.value.vec2.y = 20.0f;
     godot_set_variant(label, "position", va);
+    godot_add_child_deferred(parent, label);
 
-    void *main_node = godot_get_node("/root/Main");
-    godot_add_child_deferred(main_node, label);
-
+    // get an existing Button and connect the callfunction to the signal pressed
     void* button = godot_get_node("/root/Main/Button");
     godot_connect(button, "pressed", on_button_pressed, NULL);
 
+    // create a timernode from code
     void* timer = godot_create("Timer");
     GDExtensionVariant vt1;
     vt1.type = VARTYPE_FLOAT;
@@ -56,9 +81,19 @@ void main() {
     vt2.value.f = 1;
     godot_set_variant(timer, "autostart", vt2);
     godot_connect(timer, "timeout", on_timeout, NULL);
-    godot_add_child_deferred(testnode, timer);
+    godot_add_child_deferred(parent, timer);
+
+    long start_time = godot_get_ticks_msec();
+	benchmark();
+	long end_time = godot_get_ticks_msec();
+	long result = end_time - start_time;
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "%ld", result);
+    godot_print("GDTinyCC time to run in ms:");
+    godot_print(buffer);
 }
 
+// _ready-function is called from godot
 void _ready() {
     godot_print("GDTinyCC _ready called!");
 }
